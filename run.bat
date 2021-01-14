b01c949db9a6a03a0f50991611391a600ef1f6c4
jayarambhatb <bhat28@rediffmail.com>
2021-01-14 22:48:27 +0530

    this is my first commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+pushd F:\Selenium\JAVA\Eclipse\eclipse\maven_inetBankingProjectV2
+mvn clean install
---
+++ src/main/java/maven_inetBankingProjectV2/maven_inetBankingProjectV2/App.java
@@ -0,0 +1,13 @@
+package maven_inetBankingProjectV2.maven_inetBankingProjectV2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
